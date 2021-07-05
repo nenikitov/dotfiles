cde931b678d04aa7fd8d33e293cae81308ea3859
nenikitov <nenikitov@mail.ru>
2021-07-05 17:19:49 -0400

    Added icons to taglist

--- modules/deco/iconlist.lua
+++ modules/deco/iconlist.lua
@@ -1,7 +1,11 @@
 local icons = {
-    ['code'] = '\u{f121}',
-    ['cpu']  = '\u{f2db}',
-    ['ram']  = '\u{f538}'
+    -- This table corresponds to unicode indexes of icons in Font Awesome. It may be different for other icon fonts
+    ["home"]     = "\u{f015}",
+    ["terminal"] = "\u{f120}",
+    ["code"]     = "\u{f121}",
+    ["brush"]    = "\u{f1fc}",
+    ["study"]    = "\u{f19d}",
+    ["folder"]   = "\u{f07b}",
 }
 
 return icons
--- modules/main/tags.lua
+++ modules/main/tags.lua
@@ -1,25 +1,32 @@
 -- Standard Awesome libraries
 local awful = require("awful")
+local iconlist = require("modules.deco.iconlist")
 
 -- Init layouts
 local ls = awful.layout.suit
 
 local tags = {}
-local tagnames = { "chill", "term", "code", "draw", "study", "fun", "7", "8", "9" }
+local tagtext = { iconlist["home"], iconlist["terminal"], iconlist["code"], iconlist["brush"], iconlist["study"], "fun", "7", "8", "9" }
 local taglayouts = ls.floating
 
 -- Add layouts to each screen
 awful.screen.connect_for_each_screen(
     function(s)
-        for i in pairs(tagnames) do
-            awful.tag.add(tagnames[i], {
-                -- icon = "/path/to/icon.png",
-                screen = s,
-                layout = taglayouts[i],
-                gap_single_client = true,
-                selected = (i == 1)
-            })
-        end
+        awful.tag(tagtext, s, taglayouts)
+
+        -- for i in pairs(tagnames) do
+        --     awful.tag.add(tagnames[i], {
+        --         -- icon = "/path/to/icon.png",
+        --         -- icon = "/home/nenikitov/Downloads/test-icon.png",
+        --         screen = s,
+        --         layout = taglayouts[i],
+        --         gap_single_client = true,
+        --         selected = (i == 1)
+        --     })
+        -- end
+
+        -------
+
         -- awful.tag(tagnames, s, taglayouts)
     end
 )
