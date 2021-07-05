2d72d400aa09144ae8336cd682ece37a3069ed8f
nenikitov <nenikitov@mail.ru>
2021-07-05 01:26:25 -0400

    Remade tag initialization logic

--- modules/main/tags.lua
+++ modules/main/tags.lua
@@ -9,8 +9,19 @@ local tagnames = { "chill", "term", "code", "draw", "study", "fun", "7", "8", "9
 local taglayouts = ls.floating
 
 -- Add layouts to each screen
-awful.screen.connect_for_each_screen(function(s)
-    awful.tag(tagnames, s, taglayouts)
-end)
+awful.screen.connect_for_each_screen(
+    function(s)
+        for i in pairs(tagnames) do
+            awful.tag.add(tagnames[i], {
+                -- icon = "/path/to/icon.png",
+                screen = s,
+                layout = taglayouts[i],
+                gap_single_client = true,
+                selected = (i == 1)
+            })
+        end
+        -- awful.tag(tagnames, s, taglayouts)
+    end
+)
 
 return tags
