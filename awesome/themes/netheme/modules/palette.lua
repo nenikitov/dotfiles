9d29ff777946c49390cc20d46de71732349b99e6
nenikitov <nenikitov@mail.ru>
2021-08-25 23:27:45 -0400

    Eduted the palette and formated color config file

--- awesome/themes/netheme/modules/palette.lua
+++ awesome/themes/netheme/modules/palette.lua
@@ -1,48 +1,36 @@
 local palette = {
     fg_text = {
-        dull   = "#909090",
-        normal = "#bfbfbf",
-        active = "#ffffff"
+        dull    = "#909090",
+        normal  = "#bfbfbf",
+        active  = "#ffffff"
+    },
+    fg_icons = {
+        red     = "#de4b55",
+        green   = "#79e43c",
+        yellow  = "#f7ab38",
+        blue    = "#478ff3",
+        magenta = "#f43bf4",
+        cyan    = "#40f9bb"
     },
     bg_neutral = {
-        dull   = "#0a0a0a",
-        normal = "#1a1a1a",
-        active = "#2a2a2a"
-    },
-    bg_variation  = {
-        dull   = "#131313",
-        normal = "#262626",
-        active = "#353535"
+        dull    = "#0a0a0a",
+        normal  = "#1a1a1a",
+        active  = "#2a2a2a"
     },
     bg_primary = {
-        dull   = "#271f18",
-        normal = "#271f18",
-        active = "#45372b"
+        dull    = "#271f18",
+        normal  = "#271f18",
+        active  = "#45372b"
     },
     bg_secondary = {
-        dull   = "#19211b",
-        normal = "#304034",
-        active = "#3d5142"
+        dull    = "#19211b",
+        normal  = "#304034",
+        active  = "#3d5142"
     },
     bg_tertiary = {
-        dull   = "#111d21",
-        normal = "#1f353b",
-        active = "#2b4a52"
-    },
-    bg_red = {
-        dull   = "#900000",
-        normal = "#bf0000",
-        active = "#ff0000"
-    },
-    bg_green = {
-        dull   = "#009000",
-        normal = "#00bf00",
-        active = "#00ff00"
-    },
-    bg_blue = {
-        dull   = "#000090",
-        normal = "#0000bf",
-        active = "#0000ff"
+        dull    = "#111d21",
+        normal  = "#1f353b",
+        active  = "#2b4a52"
     }
 }
 
--- awesome/themes/netheme/modules/parameters.lua
+++ awesome/themes/netheme/modules/parameters.lua
@@ -3,51 +3,59 @@ theme.rounding_buttons = 6
 theme.rounding_clients = 10
 
 -- Font
-theme.font          = "sans 12"
+theme.font          = "Jost* 12"
 
+-- Decorations
+theme.useless_gap   = dpi(5)
+theme.border_width  = dpi(1)
+
+-- {{{ Generic colors
 -- Foreground colors
 theme.fg_normal     = palette.fg_text.normal
 theme.fg_focus      = palette.fg_text.active
 theme.fg_urgent     = palette.fg_text.active
 theme.fg_minimize   = palette.fg_text.active
 -- Background colors
-theme.bg_normal     = palette.bg_blue.dull .. "f2"
-theme.bg_focus      = palette.bg_red.active
+theme.bg_normal     = palette.bg_neutral.dull .. "f2"
+theme.bg_focus      = palette.bg_primary.active
 theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = palette.bg_variation.active
+theme.bg_minimize   = palette.bg_neutral.active
 theme.bg_systray    = theme.bg_normal
 -- Borders
-theme.border_width  = dpi(1)
-theme.border_normal = palette.bg_blue.dull
-theme.border_focus  = palette.bg_red.active
+theme.border_normal = palette.bg_neutral.dull
+theme.border_focus  = palette.bg_primary.active
 theme.border_marked = "#91231c"
+-- }}}
 
--- Gap
-theme.useless_gap   = dpi(5)
-
--- Variables set for theming the menu:
+-- {{{ More specific colors
+-- Menu
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(30)
 theme.menu_width  = dpi(200)
 
--- Variables set for theming notifications:
+-- Notifications
 -- notification_font
 -- notification_[bg|fg]
 -- notification_[width|height|margin]
 -- notification_[border_color|border_width|shape|opacity]
 
-
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
+-- Taglist
 -- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+
+-- Tasklist
 -- tasklist_[bg|fg]_[focus|urgent]
+
+-- Titlebar
 -- titlebar_[bg|fg]_[normal|focus]
+
+-- Tooltip
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+
+-- Prompt
 -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+
+-- Hotkeys
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
+-- }}}
