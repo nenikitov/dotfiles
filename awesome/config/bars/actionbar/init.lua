6d522969f0664e9d20f7f6940d25d741c7ca209e
nenikitov <nenikitov@mail.ru>
2021-09-02 19:26:23 -0400

    Refactored action bar

--- awesome/config/bars/actionbar/init.lua
+++ awesome/config/bars/actionbar/init.lua
@@ -13,56 +13,48 @@ local taglist_buttons = require('config.bars.actionbar.widgets.taglist_buttons')
 local tasklist_buttons = require('config.bars.actionbar.widgets.tasklist_buttons')()
 local layoutbox_buttons = require('config.bars.actionbar.widgets.layoutbox_buttons')()
 
--- {{{ Generate widgets that are the same on all the screens
--- Launcher menu
+
+-- Widgets that are the same on all screen
 local launcher = awful.widget.launcher({
     image = beautiful.awesome_icon,
     menu = user_menu
 })
--- Current keyboard layout
 local keyboardlayout = awful.widget.keyboardlayout()
--- Clock
 local textclock = wibox.widget.textclock()
--- Systray for background applications
 local systray = wibox.widget.systray()
--- }}}
 
--- {{{ Send button info to the theme
+
+-- Send button info to the theme
 beautiful.tasklist_buttons = tasklist_buttons
 beautiful.taglist_buttons = taglist_buttons
--- }}}
 
 
+-- Set up the action bar for each screen
 awful.screen.connect_for_each_screen(
-    -- Set up the status bar (top bar) for each screen
     function(s)
-        -- {{{ Generate widgets that are unique for each screen
-        -- Promptbox 
+        -- Generate widgets that are unique for each screen
         s.promptbox = awful.widget.prompt()
-        -- Taglist
         s.taglist = awful.widget.taglist {
             screen = s,
             filter = awful.widget.taglist.filter.all,
             buttons = taglist_buttons,
         }
-        -- Tasklist
         s.tasklist = awful.widget.tasklist {
             screen = s,
             filter = awful.widget.tasklist.filter.currenttags,
             buttons = tasklist_buttons,
         }
-        -- Current layout indicator
         s.layoutbox = awful.widget.layoutbox(s)
         s.layoutbox:buttons(layoutbox_buttons)
-        -- Wibar on top that will display previous widgets
+        
+        -- Generate the container
         s.wibox = awful.wibar {
             position = 'top',
             screen = s,
-            height = 30,
         }
-        -- }}}
 
-        -- Add widgets to the wibox
+
+        -- Add widgets to the wibar
         s.wibox:setup {
             -- Left widgets
             {
@@ -87,7 +79,7 @@ awful.screen.connect_for_each_screen(
             layout = wibox.layout.align.horizontal,
         }
 
-        -- Let the theme recreate wibox
+        -- Let the theme modify the wibar
         if beautiful.at_screen_connect ~= nil
         then
             beautiful.at_screen_connect(s)
