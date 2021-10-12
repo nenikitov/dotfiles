ba9cfebc3ca124207a2d7e397b52129a6d03b4c3
nenikitov <nenikitov@mail.ru>
2021-10-12 15:52:22 -0400

    Figuring out how calendar style works

--- awesome/neconfig/config/popups/calendar/widgets/calendar_init.lua
+++ awesome/neconfig/config/popups/calendar/widgets/calendar_init.lua
@@ -8,36 +8,46 @@ local font = beautiful.font
 local calendar_style_lookup = {
     -- Whole month container
     month = {
-
+        bg = '#0000'
     },
     -- Normal days
     normal = {
-
+        bg = '#0000'
     },
     -- Current day
     focus = {
-
+        bg = '#fff4'
     },
     -- Header (month name)
     header = {
-
+        bg = '#0000'
     },
     -- Header (name of the days)
     weekday = {
-
+        bg = '#0000'
     }
 }
-local function calendar_style(widget, flag, date)
-    return widget
+local function calendar_style(contents, flag, date)
+    local props = calendar_style_lookup[flag]
+    return wibox.widget {
+        contents,
+
+        bg = props.bg,
+        widget = wibox.container.background
+    }
 end
 
 return wibox.widget {
-    date = os.date('*t'),
-    font = font,
-    spacing = 10,
-    week_numbers = false,
-    start_sunday = false,
-    long_weekdays = false,
-    fn_embed = calendar_style,
-    widget = wibox.widget.calendar.month
+    {
+        date = os.date('*t'),
+        font = font,
+        spacing = 10,
+        week_numbers = false,
+        start_sunday = false,
+        long_weekdays = false,
+        fn_embed = calendar_style,
+        widget = wibox.widget.calendar.month
+    },
+
+    widget = wibox.container.place
 }
