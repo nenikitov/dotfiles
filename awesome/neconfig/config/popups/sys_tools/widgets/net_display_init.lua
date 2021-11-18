3ce1cd04e03d279506505974d946d077973ff320
nenikitov <nenikitov@mail.ru>
2021-11-18 12:56:28 -0500

    Created network widget

---
+++ awesome/neconfig/config/popups/sys_tools/widgets/net_display_init.lua
@@ -0,0 +1,23 @@
+-- Load libraries
+local vicious = require('vicious')
+local wibox = require('wibox')
+-- Load custom modules
+require('neconfig.config.utils.widget_utils')
+local icons = require('neconfig.config.utils.icons')
+
+local download = create_monitor_widget(
+    vicious.widgets.net, 1,
+    '${enp0s31f6 down_mb}', icons.download
+)
+
+local upload = create_monitor_widget(
+    vicious.widgets.net, 1,
+    '${enp0s31f6 up_mb}', icons.upload
+)
+
+return wibox.widget {
+    download,
+    upload,
+
+    layout = wibox.layout.flex.horizontal
+}
