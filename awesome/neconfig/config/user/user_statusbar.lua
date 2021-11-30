f2d8dfe09b1e069a63e6c152a1482585fa21c714
nenikitov <nenikitov@mail.ru>
2021-11-30 17:21:54 -0500

    Started separating config files

---
+++ awesome/neconfig/config/user/user_statusbar.lua
@@ -0,0 +1,69 @@
+-- █▀ ▀█▀ ▄▀█ ▀█▀ █ █ █▀ █▄▄ ▄▀█ █▀█
+-- ▄█  █  █▀█  █  █▄█ ▄█ █▄█ █▀█ █▀▄
+local statusbar = {
+    -- Show the status bar by default (statusbar can be toggled with a keyboard shortcut)
+    visible = true,
+    -- Widget settings
+    widgets = {
+        -- Menu widget
+        menu = {
+            -- Should be shown
+            visible = true
+        },
+        -- Run menu widget
+        run_menu = {
+            -- Should be shown
+            visible = false
+        },
+        -- Current desktop layout
+        layoutbox = {
+            -- Should be shown
+            visible = true
+        },
+        -- List of tags (virtual desktops)
+        taglist = {
+            visible = true,
+            -- Should tags be numbered
+            number = false,
+            -- Should the client count indicator (small dots under the tag) be shown
+            show_client_count = true
+        },
+        -- List of opened clients
+        tasklist = {
+            -- Should be shown
+            visible = true,
+            -- Should the name of the task be shown
+            show_task_title = true,
+            -- Should the property (maximized, on top, etc) be shown
+            show_task_props = true
+        },
+        -- Analog clock
+        clock = {
+            -- Should be shown
+            visible = true,
+            -- Format of the time written on the top or left
+            primary_format = '%H:%M',
+            -- Format of the time written on the bottom or right
+            secondary_format = '%a %Y-%m-%d',
+        },
+        -- Current keyboard layout
+        keyboard_layout = {
+            -- Should be shown
+            visible = false,
+        },
+        -- Notifications pane
+        notifications_pane = {
+            -- Should be shown
+            visible = true
+        },
+        -- System tools
+        sys_tools = {
+            -- Should be shown
+            visible = true,
+            -- Name of the thermal 
+            cpu_thermal_zone='thermal_zone1'
+        }
+    },
+}
+
+return statusbar
