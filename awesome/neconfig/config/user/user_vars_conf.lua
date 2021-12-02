100d8c5fb6838f3b164ecc23086b9f391a01f5cf
nenikitov <nenikitov@mail.ru>
2021-12-01 22:40:58 -0500

    Further separation of the config file

--- awesome/neconfig/config/user/user_vars_conf.lua
+++ awesome/neconfig/config/user/user_vars_conf.lua
@@ -51,120 +51,12 @@ local binds = {
     }
 }
 
--- █▀▄ █▀▀ █▀ █▄▀ ▀█▀ █▀█ █▀█
--- █▄▀ ██▄ ▄█ █ █  █  █▄█ █▀▀
-local desktop = {
-    -- Path to the theme
-    theme_path = config_path .. 'neconfig/theme/theme.lua',
-    -- Names (and consequently number) of tags
-    tag_names = {
-        icons.home,
-        icons.terminal,
-        icons.code,
-        icons.globe,
-        icons.media,
-        icons.communication,
-    },
-    -- List of layouts that can be cycled through
-    -- (the first is selected by default)
-    layouts = {
-        awful.layout.suit.spiral,
-        awful.layout.suit.tile,
-        awful.layout.suit.floating,
-
-        --[[ All available layouts
-            awful.layout.suit.tile
-            awful.layout.suit.tile.left
-            awful.layout.suit.tile.bottom
-            awful.layout.suit.tile.top
-            awful.layout.suit.fair
-            awful.layout.suit.fair.horizontal
-            awful.layout.suit.floating
-            awful.layout.suit.spiral
-            awful.layout.suit.spiral.dwindle
-            awful.layout.suit.max
-            awful.layout.suit.max.fullscreen
-            awful.layout.suit.magnifier
-            awful.layout.suit.corner.nw
-            awful.layout.suit.corner.ne
-            awful.layout.suit.corner.sw
-            awful.layout.suit.corner.se
-        -- ]]
-    }
-}
-
--- █▀ ▀█▀ ▄▀█ ▀█▀ █ █ █▀ █▄▄ ▄▀█ █▀█
--- ▄█  █  █▀█  █  █▄█ ▄█ █▄█ █▀█ █▀▄
-local statusbar = {
-    -- Show the status bar by default (statusbar can still be toggled with a keyboard shortcut)
-    visible = true,
-    -- Widget settings
-    widgets = {
-        -- Open menu widget
-        menu = {
-            -- Should it be shown
-            visible = true
-        },
-        -- Open run menu widget
-        run_menu = {
-            -- Should it be shown
-            visible = false
-        },
-        -- Current desktop layout
-        layoutbox = {
-            -- Should it be shown
-            visible = true
-        },
-        -- List of tags (virtual desktops)
-        taglist = {
-            -- Should it be shown
-            visible = true,
-            -- Should tags be numbered
-            number = false,
-            -- Should the small dots under the tag be shown to indicate the number of tagged clients
-            show_client_count = true
-        },
-        -- List of tasks (opened apps)
-        tasklist = {
-            -- Should it be shown
-            visible = true,
-            -- Should the name of the task be shown
-            show_task_title = true,
-            -- Should the property (maximized, on top, etc) be shown
-            show_task_props = true
-        },
-        -- Analog clock
-        clock = {
-            -- Should it be shown
-            visible = true,
-            -- Format of the time written on the top or left
-            primary_format = '%H:%M',
-            -- Format of the time written on the bottom or right
-            secondary_format = '%a %Y-%m-%d',
-        },
-        -- Current keyboard layout
-        keyboard_layout = {
-            -- Should it be shown
-            visible = false,
-        },
-        -- Notifications pane
-        notifications_pane = {
-            visible = true
-        },
-        -- System tools
-        sys_tools = {
-            visible = true,
-            cpu_thermal_zone='thermal_zone1'
-        }
-    },
-}
-
 
 local user_vars_conf = {
     apps = apps,
     binds = binds,
-    desktop = desktop,
-    statusbar = statusbar
+    desktop = require('neconfig.config.user.user_desktop'),
+    statusbar = require('neconfig.config.user.user_statusbar')
 }
 
 return user_vars_conf
