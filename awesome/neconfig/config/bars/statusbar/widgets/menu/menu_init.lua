65d7406e97d31f84c00fc2c6974d3dc8a44ecfb5
nenikitov <nenikitov@mail.ru>
2021-12-26 11:10:33 -0500

    Refactored menus

--- awesome/neconfig/config/bars/statusbar/widgets/menu/menu_init.lua
+++ awesome/neconfig/config/bars/statusbar/widgets/menu/menu_init.lua
@@ -5,14 +5,16 @@ local beautiful = require('beautiful')
 -- Load custom modules
 local menu_user_conf = require('neconfig.config.user.menu_user_conf')
 
-
+-- Get variables
 local font_height = beautiful.get_font_height(beautiful.font)
--- Construct menu widget
+
+
+-- Construct the widget
 local launcher = awful.widget.launcher {
     image = beautiful.system_icon,
     menu = menu_user_conf
 }
-
+-- Launcher does not accept these parameters in the constructor
 launcher.forced_width = font_height * 0.75
 launcher.forced_height = font_height * 0.75
 launcher.halign = 'center'
