3ded86fd9c0ad301e36b36244d0929c8bd19ff9b
nenikitov <nenikitov@mail.ru>
2021-12-29 21:57:52 -0500

    Further refactoring in tasklist

--- awesome/neconfig/config/bars/statusbar/widgets/tasklist/tasklist_init.lua
+++ awesome/neconfig/config/bars/statusbar/widgets/tasklist/tasklist_init.lua
@@ -2,27 +2,12 @@
 local awful = require('awful')
 -- Load custom modules
 local tasklist_buttons = require('neconfig.config.bars.statusbar.widgets.tasklist.tasklist_buttons')
-local statusbar_user_conf = require('neconfig.config.user.statusbar_user_conf')
 
 
 -- Generate tasklist widget
-local function get_tasklist(screen, bar_info)
-    -- Get style from the theme
-    local widget_info = bar_info.widgets.tasklist
-    local style = {
-        bar_pos = bar_info.position,
-        corner_radius = bar_info.corner_radius.sections,
-        size = bar_info.contents_size,
-        decoration_size = widget_info.decoration_size,
-        padding = widget_info.padding,
-        spacing = widget_info.spacing,
-        task_size = widget_info.task_size,
-        max_size = widget_info.max_size,
-        show_task_title = statusbar_user_conf.widgets.tasklist.show_task_title
-    }
-
+local function get_tasklist(screen, args)
     -- Get custom widget properties
-    local tasklist_widget = require('neconfig.config.bars.statusbar.widgets.tasklist.tasklist_widget')(style)
+    local tasklist_widget = require('neconfig.config.bars.statusbar.widgets.tasklist.tasklist_widget')(args)
 
     -- Construct widget
     local tasklist = awful.widget.tasklist {
