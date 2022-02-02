7f8165e2bea251cb09c00341833a35c4fdccbc05
nenikitov <nenikitov@mail.ru>
2022-02-02 13:03:21 -0500

    Moving statusbar configs

---
+++ awesome/neconfig/user/widgets/user_statusbar.lua
@@ -0,0 +1,36 @@
+-- Load custom modules
+local statusbar_subwidget_list = require('neconfig.config.widgets.statusbar.statusbar_subwidget_list')
+
+
+-- █▀ ▀█▀ ▄▀█ ▀█▀ █ █ █▀   █▄▄ ▄▀█ █▀█
+-- ▄█  █  █▀█  █  █▄█ ▄█   █▄█ █▀█ █▀▄
+-- Status bars on the primary screen
+local primary = {
+    top = {
+        visible = true,
+        layout = {
+            first = {
+                statusbar_subwidget_list.menu,
+                statusbar_subwidget_list.run_menu,
+                statusbar_subwidget_list.layout_box,
+                statusbar_subwidget_list.tag_list
+            },
+            middle = {
+                statusbar_subwidget_list.task_list
+            },
+            last = {
+                statusbar_subwidget_list.text_clock,
+                statusbar_subwidget_list.keyboard,
+                statusbar_subwidget_list.notification_center,
+                statusbar_subwidget_list.system_tools
+            }
+        }
+    }
+}
+-- Status bars on the other screens
+local others = primary
+
+return {
+    primary = primary,
+    others = others
+}
