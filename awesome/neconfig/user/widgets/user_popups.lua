a2228c8c82a5ed67356393e0781f5b74b10ca5c8
nenikitov <nenikitov@mail.ru>
2022-02-03 08:45:41 -0500

    Created popup configuration

---
+++ awesome/neconfig/user/widgets/user_popups.lua
@@ -0,0 +1,23 @@
+-- █▀█ █▀█ █▀█ █ █ █▀█ █▀
+-- █▀▀ █▄█ █▀▀ █▄█ █▀▀ ▄█`
+local popups = {
+    -- Notifications
+    notification_popup = {
+        timeout = 5,
+        position = 'top_right'
+    },
+    -- Popup when changing volume
+    volume_popup = {
+        show = true,
+        timeout = 2,
+        position = 'right'
+    },
+    -- Popup when changing brightness
+    brightness_popup = {
+        show = true,
+        timeout = 2,
+        position = 'right'
+    }
+}
+
+return popups
