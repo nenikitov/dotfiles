e076d34f10f6bbc706348d12ba79dcfc0fb49d25
nenikitov <nenikitov@mail.ru>
2022-02-02 23:19:25 -0500

    Added widget settings for statusbar

--- awesome/neconfig/user/widgets/user_statusbar.lua
+++ awesome/neconfig/user/widgets/user_statusbar.lua
@@ -6,19 +6,26 @@ local statusbar_subwidget_list = require('neconfig.config.widgets.statusbar.stat
 -- ▄█  █  █▀█  █  █▄█ ▄█   █▄█ █▀█ █▀▄
 -- Status bars on the primary screen
 local primary = {
+    -- Bar on top of the screen
     top = {
+        -- Visible by default (if hidden can still be toggled with the shortcut)
         visible = true,
+        -- Is drawn on top of the clients
         on_top = false,
+        -- Sub widgets
         layout = {
+            -- Start of the bar (left)
             beginning = {
                 statusbar_subwidget_list.menu,
                 statusbar_subwidget_list.run_menu,
                 statusbar_subwidget_list.layout_box,
                 statusbar_subwidget_list.tag_list
             },
+            -- Center of the bar (middle)
             center = {
                 statusbar_subwidget_list.task_list
             },
+            -- End of the bar (right)
             ending = {
                 statusbar_subwidget_list.text_clock,
                 statusbar_subwidget_list.keyboard,
