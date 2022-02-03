c05fa19beb95fc7cbcd6ed75e689e9ccf98c788a
nenikitov <nenikitov@mail.ru>
2022-02-03 08:50:44 -0500

    Added comments to popup config

--- awesome/neconfig/user/widgets/user_popups.lua
+++ awesome/neconfig/user/widgets/user_popups.lua
@@ -3,19 +3,27 @@
 local popups = {
     -- Notifications
     notification_popup = {
+        -- Time in seconds before popup disappears
         timeout = 5,
+        -- Position on the screen
         position = 'top_right'
     },
     -- Popup when changing volume
     volume_popup = {
+        -- Show the popup when changing volume
         show = true,
+        -- Time in seconds before popup disappears
         timeout = 2,
+        -- Position on the screen
         position = 'right'
     },
     -- Popup when changing brightness
     brightness_popup = {
+        -- Show the popup when changing brightness
         show = true,
+        -- Time in seconds before popup disappears
         timeout = 2,
+        -- Position on the screen
         position = 'right'
     }
 }
