cae5be12c086bd214bd9d2e8ee6c814c593f5aac
nenikitov <nenikitov@mail.ru>
2022-02-04 08:57:59 -0500

    Started porting theme stuff

---
+++ awesome/neconfig/user/theme/user_scaling.lua
@@ -0,0 +1,12 @@
+-- █▀ █▀▀ ▄▀█ █   █ █▄ █ █▀▀
+-- ▄█ █▄▄ █▀█ █▄▄ █ █ ▀█ █▄█
+local scaling = {
+    -- Multiplier for the scale of the contents
+    contents = 1.0,
+    -- Multiplier for the scale of the spacing in between the contents
+    spacing = 1.0,
+    -- Multiplier for the scale of the cutout in the corners
+    corners = 1.0
+}
+
+return scaling
