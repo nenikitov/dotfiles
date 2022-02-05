6a1bf74e0816abac1d322d4c63c39a15b86be3c9
nenikitov <nenikitov@mail.ru>
2022-02-05 11:47:03 -0500

    Added comments to color classes

--- awesome/neconfig/user/theme/user_colors.lua
+++ awesome/neconfig/user/theme/user_colors.lua
@@ -36,16 +36,16 @@ local scheme = {
         bg = palette.bg.normal,
         fg = palette.fg.normal
     },
-    -- "Stand out" elements on top of neutral background
+    -- Highlighted elements on top of neutral background
     surface = {
         bg = palette.bg.black,
         fg = palette.fg.white
     },
-    -- Name of the color from the palette used as primary "stand out" color
+    -- Name of the color from the palette used as primary color
     primary   = 'blue',
-    -- Name of the color from the palette used as secondary "stand out" color
+    -- Name of the color from the palette used as secondary color
     secondary = 'magenta',
-    -- Name of the color from the palette used as tertiary "stand out" color
+    -- Name of the color from the palette used as tertiary color
     tertiary  = 'yellow'
 }
 
@@ -53,35 +53,55 @@ local scheme = {
 -- █▀▀ █▀█ █   █▀█ █▀█   █▀▀ █   ▄▀█ █▀ █▀ █▀▀ █▀
 -- █▄▄ █▄█ █▄▄ █▄█ █▀▄   █▄▄ █▄▄ █▀█ ▄█ ▄█ ██▄ ▄█
 local classes = {
-    neutral = scheme.neutral,
-    surface = scheme.surface,
+    --#region Neutral
 
+    -- Normal neutral colors
+    neutral = scheme.neutral,
+    -- Highlighted elements on top of neutral background
+    surface = scheme.surface,
+    --#endregion
+
+    --#region Primary
+
+    -- Colors when primary is used for foreground
     primary = {
         bg = scheme.neutral.bg,
         fg = palette.fg[scheme.primary]
     },
+    -- Colors when primary is used for background
     primary_surface = {
         bg = palette.bg[scheme.primary],
         fg = scheme.surface.fg
     },
+    --#endregion
 
+    --#region Secondary
+
+    -- Colors when secondary is used for foreground
     secondary = {
         bg = scheme.neutral.bg,
         fg = palette.fg[scheme.secondary]
     },
+    -- Colors when secondary is used for background
     secondary_surface = {
         bg = palette.bg[scheme.secondary],
         fg = scheme.surface.fg
     },
+    --#endregion
 
+    --#region Tertiary
+
+    -- Colors when tertiary is used for foreground
     tertiary = {
         bg = scheme.neutral.bg,
         fg = palette.fg[scheme.tertiary]
     },
+    -- Colors when tertiary is used for background
     tertiary_surface = {
         bg = palette.bg[scheme.tertiary],
         fg = scheme.surface.fg
     },
+    --#endregion
 }
 
 return {
