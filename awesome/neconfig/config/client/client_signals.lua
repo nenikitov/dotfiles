8de03042e33d5b011b32a7c89ea322dd114f1cb1
nenikitov <nenikitov@mail.ru>
2022-02-06 12:01:41 -0500

    Improved icon rendering

--- awesome/neconfig/config/client/client_signals.lua
+++ awesome/neconfig/config/client/client_signals.lua
@@ -15,19 +15,25 @@ client.connect_signal(
     'manage',
     function (c)
         -- Set the windows at the slave (put it at the end of others instead of setting it master)
-        if (awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position)
-        then
+        if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
             -- Prevent clients from being unreachable after screen count changes.
             awful.placement.no_offscreen(c)
         end
 
         -- Force set GTK icon
-        if (user_vars_theme.client.try_to_force_icon_theme)
+        if user_vars_theme.client.try_to_force_icon_theme
         then
-            local icon = menubar_utils.lookup_icon(c.instance)
-            if (icon ~= nil)
-            then
-                c.icon = gears.surface(icon)._native
+            if c.instance ~= nil then
+                local icon = menubar_utils.lookup_icon(c.instance)
+                local lower_icon = menubar_utils.lookup_icon(c.instance:lower())
+
+                if icon ~= nil then
+                    c.icon = gears.surface(icon)._native
+                elseif lower_icon ~= nil then
+                    c.icon = gears.surface(lower_icon)._native
+                end
+            elseif c.icon == nil then
+                c.icon = gears.surface(menubar_utils.lookup_icon('application-default-icon'))._native
             end
         end
     end
