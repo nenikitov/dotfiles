b47c448b2174663371af922d58f5c555d7f00af0
nenikitov <nenikitov@mail.ru>
2022-02-08 12:50:35 -0500

    Created better rectangle shape

--- awesome/neconfig/config/utils/utils_shapes.lua
+++ awesome/neconfig/config/utils/utils_shapes.lua
@@ -6,38 +6,92 @@ local rad = math.rad
 local utils_shapes = {}
 
 function utils_shapes.better_rect(args)
-    local radius = args.radius
+    args = args or {}
+
+    local def_radius = 0
+    local radius = args.radius or def_radius
     radius =
         type(radius) == 'table'
         and {
-            tr = radius.tr or 0,
-            tl = radius.tl or 0,
-            br = radius.br or 0,
-            bl = radius.bl or 0
+            tr = radius.tr or def_radius,
+            tl = radius.tl or def_radius,
+            br = radius.br or def_radius,
+            bl = radius.bl or def_radius
         }
         or {
-            tr = radius or 0,
-            tl = radius or 0,
-            br = radius or 0,
-            bl = radius or 0
+            tr = radius or def_radius,
+            tl = radius or def_radius,
+            br = radius or def_radius,
+            bl = radius or def_radius
         }
 
-    local cutout = args.cutout
+    local def_cutout = 'round'
+    local cutout = args.cutout or def_cutout
     cutout =
-        type(radius) == 'table'
+        type(cutout) == 'table'
         and {
-            tr = cutout.tr or 'cutout',
-            tl = cutout.tl or 'cutout',
-            br = cutout.br or 'cutout',
-            bl = cutout.bl or 'cutout'
+            tr = cutout.tr or def_cutout,
+            tl = cutout.tl or def_cutout,
+            br = cutout.br or def_cutout,
+            bl = cutout.bl or def_cutout
         }
         or {
-            tr = cutout or 'none',
-            tl = cutout or 'none',
-            br = cutout or 'none',
-            bl = cutout or 'none'
+            tr = cutout or def_cutout,
+            tl = cutout or def_cutout,
+            br = cutout or def_cutout,
+            bl = cutout or def_cutout
         }
 
+    return function(cr, w, h)
+        cr:move_to(0, radius.tl)
+
+        -- Top left corner
+        if cutout.tl == 'round' then
+            cr:arc(
+                radius.tl, radius.tl,
+                radius.tl, rad(180), rad(270)
+            )
+        else
+            cr:line_to(radius.tl, 0)
+        end
+
+        -- Top right corner
+        if cutout.tr == 'round' then
+            cr:arc(
+                w - radius.tr, radius.tr,
+                radius.tr, rad(270), rad(0)
+            )
+        else
+            cr:line_to(w - radius.tr, 0)
+            cr:line_to(w, radius.tr)
+        end
+
+        -- Bottom right corner
+        if cutout.br == 'round' then
+            cr:arc(
+                w - radius.br, h - radius.br,
+                radius.br, rad(0), rad(90)
+            )
+        else
+            cr:line_to(w, h - radius.br)
+            cr:line_to(w - radius.br, h)
+        end
+
+        -- Bottom left corner
+        if cutout.bl == 'round' then
+            cr:arc(
+                radius.bl, h - radius.bl,
+                radius.bl, rad(90), rad(180)
+            )
+        else
+            cr:line_to(radius.br, h)
+            cr:line_to(0, h - radius.br)
+        end
+
+
+
+        cr:close_path()
+    end
 end
 
 return utils_shapes
