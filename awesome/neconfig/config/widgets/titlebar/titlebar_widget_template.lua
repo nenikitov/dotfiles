2b437fb7a07f57b270eeb824c8497fad44774c46
nenikitov <nenikitov@mail.ru>
2022-02-11 12:55:25 -0500

    Fixed buttons

--- awesome/neconfig/config/widgets/titlebar/titlebar_widget_template.lua
+++ awesome/neconfig/config/widgets/titlebar/titlebar_widget_template.lua
@@ -10,11 +10,23 @@ local titlebar_buttons = require("neconfig.config.widgets.titlebar.titlebar_butt
 local function titlebar_widget_template(c)
     local direction = utils_shapes.direction_of_side(user_titlebar.position)
     local align = utils_shapes.alignments(direction)
-    local init_widget = function(widget)
-        return widget(c)
-    end
     local buttons = titlebar_buttons(c)
+    -- Function to init a widget inside a titlebar
+    local function init_widget(prototype)
+        if type(prototype) == 'function' then
+            -- Pass client to the widget
+            local widget = prototype(c)
+            -- Provide default buttons if has no buttons
+            if #widget:buttons() == 0 then
+                widget:buttons(buttons)
+            end
+            return widget
+        else
+            return prototype
+        end
+    end
 
+    -- Layouts for 3 sections
     local beginning_section = wibox.layout.fixed[direction](
         table.unpack(
             g_table.map(
@@ -31,7 +43,6 @@ local function titlebar_widget_template(c)
             )
         )
     )
-    center_section.buttons = buttons
     local ending_section = wibox.layout.fixed[direction](
         table.unpack(
             g_table.map(
@@ -41,19 +52,32 @@ local function titlebar_widget_template(c)
         )
     )
 
+    -- Ugly hack with spacer widget that fills the remaining space to make empty regions of titlebars intractable
+    local spacer = {
+        buttons = buttons,
+
+        widget = wibox.container.background,
+    }
+    -- Final widget
     return {
-        beginning_section,
+        {
+            beginning_section,
+            spacer,
+
+            layout = wibox.layout.align[direction]
+        },
         center_section,
         {
+            spacer,
+            spacer,
             ending_section,
 
-            [align.direction] = align.ending,
+            expand = 'inside',
 
-            widget = wibox.container.place
+            layout = wibox.layout.align[direction]
         },
 
         expand = 'outside',
-        buttons = buttons,
 
         layout = wibox.layout.align[direction]
     }
