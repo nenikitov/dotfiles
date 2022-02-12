14a56e63960c2c563c858fade21ebadcf7558c57
nenikitov <nenikitov@mail.ru>
2022-02-12 09:57:27 -0500

    Remaking the color system

--- awesome/neconfig/user/look/user_look_colors.lua
+++ awesome/neconfig/user/look/user_look_colors.lua
@@ -1,7 +1,7 @@
 -- ▀█▀ █▀▀ █▀█ █▀▄▀█ █ █▄ █ ▄▀█ █     █▀▀ █▀█ █   █▀█ █▀█   █▀█ ▄▀█ █   █▀▀ ▀█▀ ▀█▀ █▀▀
 --  █  ██▄ █▀▄ █ ▀ █ █ █ ▀█ █▀█ █▄▄   █▄▄ █▄█ █▄▄ █▄█ █▀▄   █▀▀ █▀█ █▄▄ ██▄  █   █  ██▄
 local palette = {
-    -- Colors used as background
+    -- Colors used for background
     bg = {
         normal  = '#080808',
         black   = '#3A3A3A',
@@ -13,7 +13,7 @@ local palette = {
         cyan    = '#5AA79D',
         white   = '#B0B0B0'
     },
-    -- Colors used as foreground (text, lines, etc)
+    -- Colors used for foreground (text, lines, etc)
     fg = {
         normal  = '#CCCCCC',
         black   = '#5A5A5A',
@@ -29,105 +29,43 @@ local palette = {
 }
 
 
+local function create_scheme(color)
+    return {
+        bg = palette.bg[color],
+        fg = palette.fg[color]
+    }
+end
+
+
 -- █▀▀ █▀█ █   █▀█ █▀█   █▀ █▀▀ █ █ █▀▀ █▀▄▀█ █▀▀
 -- █▄▄ █▄█ █▄▄ █▄█ █▀▄   ▄█ █▄▄ █▀█ ██▄ █ ▀ █ ██▄
 local scheme = {
-    -- Normal neutral colors
-    neutral = {
-        bg = palette.bg.normal,
-        fg = palette.fg.normal,
-        transparency = palette.transparency
-    },
-    -- Highlighted elements on top of neutral background
+    -- Transparency
+    transparency = palette.transparency,
+
+    -- Neutral colors
+    neutral = create_scheme('normal'),
     surface = {
         bg = palette.bg.black,
         fg = palette.fg.white,
-        transparency = 1.0
     },
 
-    -- Name of the color from the palette used as primary color
-    primary   = 'blue',
-    -- As secondary color
-    secondary = 'magenta',
-    -- As tertiary color
-    tertiary  = 'yellow',
+    -- Accent colors
+    primary   = create_scheme('blue'),
+    secondary = create_scheme('magenta'),
+    tertiary  = create_scheme('yellow'),
+    error     = create_scheme('red'),
 
-    -- As error color
-    error = 'red',
-
-    -- As close titlebar button color
-    close    = 'red',
-    -- As maximize titlebar button color
-    maximize = 'green',
-    -- As minimize titlebar button color
-    minimize = 'yellow',
-    -- As on top titlebar button color
-    on_top   = 'blue',
-    -- As floating titlebar button color
-    floating = 'magenta',
-    -- As sticky titlebar button color
-    sticky   = 'cyan',
-}
-
-
---#region Color class creation utilities (do not modify these)
-
----Create a color class where the specified color is used as foreground
----@param color_name string Color name in the palette
----@return table class Color class
-local function make_class_color(color_name)
-    return {
-        bg = scheme.neutral.bg,
-        fg = palette.fg[color_name],
-        transparency = scheme.neutral.transparency
-    }
-end
----Create a color class where the specified color is used as background
----@param color_name string Color name in the palette
----@return table class Color class
-local function make_class_surface(color_name)
-    return {
-        bg = scheme.bg[scheme[color_name]],
-        fg = palette.surface.fg,
-        transparency = 1.0
-    }
-end
----Create a color class where the specified color is used as background and foreground
----@param color_name string Color name in the palette
----@return table class Color class
-local function make_class_full(color_name)
-    return {
-        bg = scheme.bg[color_name],
-        fg = scheme.fg[color_name],
-        transparency = 1.0
-    }
-end
---#endregion
-
-
--- █▀▀ █▀█ █   █▀█ █▀█   █▀▀ █   ▄▀█ █▀ █▀ █▀▀ █▀
--- █▄▄ █▄█ █▄▄ █▄█ █▀▄   █▄▄ █▄▄ █▀█ ▄█ ▄█ ██▄ ▄█
-local classes = {
-    -- Neutral
-    neutral = scheme.neutral,
-    surface = scheme.surface,
-
-    -- Primary
-    primary           = make_class_color('primary'),
-    primary_surface   = make_class_surface('primary'),
-    -- Secondary
-    secondary         = make_class_color('secondary'),
-    secondary_surface = make_class_surface('secondary'),
-    -- Tertiary
-    tertiary          = make_class_color('tertiary'),
-    tertiary_surface  = make_class_surface('tertiary'),
-
-    -- Error
-    error             = make_class_color('error'),
-    error_surface     = make_class_surface('error'),
+    -- Titlebar buttons
+    close    = create_scheme('red'),
+    maximize = create_scheme('green'),
+    minimize = create_scheme('yellow'),
+    on_top   = create_scheme('blue'),
+    floating = create_scheme('magenta'),
+    sticky   = create_scheme('cyan'),
 }
 
 return {
     palette = palette,
-    classes = classes
+    scheme = scheme
 }
