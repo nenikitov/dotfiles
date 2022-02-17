d2a2c2d6e3d34f7e372dd46de3742e780d5fc5c7
nenikitov <nenikitov@mail.ru>
2022-02-16 21:33:02 -0500

    Added more applications

--- awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
+++ awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
@@ -1,11 +1,21 @@
 -- ▀█▀ █ ▀█▀ █   █▀▀ █▄▄ ▄▀█ █▀█   █▀▀ █   █ █▀▀ █▄ █ ▀█▀   █▀▀ █▀█ █   █▀█ █▀█ █▀
 --  █  █  █  █▄▄ ██▄ █▄█ █▀█ █▀▄   █▄▄ █▄▄ █ ██▄ █ ▀█  █    █▄▄ █▄█ █▄▄ █▄█ █▀▄ ▄█
 local titlebar_client_colors = {
-    ['discord'] = '#36393f',
-    ['Gimp-2.10'] = '#484848',
+    ['Qalculate-gtk'] = '#272727',
     ['com.github.joseexposito.touche'] = '#ebebebff',
+    ['firefox'] = '#1c1b22ff',
+    ['code-oss'] = '#272727',
+    ['Lxappearance'] = '#121212',
+    ['DarkPlaces'] = '#000000',
+    ['gammy'] = '#1b1c27',
+    ['Libfm-pref-apps'] = '#121212',
+    ['Pcmanfm'] = '#272727',
+    ['discord'] = '#36393f',
     ['Alacritty'] = '#080808cc',
-    ['code-oss'] = '#272727'
+    ['Gimp-2.10'] = '#484848',
+    ['Nitrogen'] = '#121212',
+    ['Electron'] = '#2f3241',
+    ['qt5ct'] = '#222020'
 }
 
 return titlebar_client_colors
