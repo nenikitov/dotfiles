d4e96eae76170f502f76ec079757b480f1eb8ce0
nenikitov <nenikitov@mail.ru>
2022-02-21 21:54:07 -0500

    Set up the propt

--- zsh/modules/prompt.zsh
+++ zsh/modules/prompt.zsh
@@ -1,17 +1,32 @@
+autoload -U colors && colors
+setopt PROMPT_SUBST
+
+precmd() {
+    if git_repo="$(git remote get-url origin 2> /dev/null)"; then
+	git_repo="$(basename $git_repo .git) - $(git rev-parse --abbrev-ref HEAD)"
+    else
+        git_repo="not a repo"
+    fi
+}
+
 prompt_tl_elems=(
-    '%n'
-    ' in '
-    '%~'
+    "%{$fg[red]%}"'%n'
+    "%{$reset_color%}"' in '
+    "%{$fg[yellow]%}"'%~'
+    "%{$reset_color%}"' git '
+    "%{$fg[green]%}"'$git_repo'
+    "%{$reset_color%}"
 )
 prompt_bl_elems=(
-    '%#'
-    ' '
-)
-prompt_tr_elems=(
-    ' '
+    "%{$fg[green]%}"'%# '
+    "%{$reset_color%}"
 )
 prompt_br_elems=(
-    ' '
+    "%{$fg[blue]%}""$(date +%H:%M:%S)"
+    "%{$reset_color%}"' '
+    "%{$fg[magenta]%}"'%?'
+    "%{$reset_color%}"
 )
 
 PS1=${(j::)prompt_tl_elems}$'\n'${(j::)prompt_bl_elems}
+RPS1=${(j::)prompt_br_elems}
