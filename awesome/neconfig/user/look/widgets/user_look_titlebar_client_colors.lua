ca32216c9eff0180ab6cc8bbecfa801c969119c2
nenikitov <nenikitov@mail.ru>
2022-03-01 16:57:49 -0500

    Removed unecessary svg file

--- awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
+++ awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
@@ -1,27 +1,28 @@
 -- ▀█▀ █ ▀█▀ █   █▀▀ █▄▄ ▄▀█ █▀█   █▀▀ █   █ █▀▀ █▄ █ ▀█▀   █▀▀ █▀█ █   █▀█ █▀█ █▀
 --  █  █  █  █▄▄ ██▄ █▄█ █▀█ █▀▄   █▄▄ █▄▄ █ ██▄ █ ▀█  █    █▄▄ █▄█ █▄▄ █▄█ █▀▄ ▄█
 local titlebar_client_colors = {
-    ['code-oss'] = '#272727',
-    ['com.github.joseexposito.touche'] = '#ebebebff',
-    ['Libfm-pref-apps'] = '#121212',
-    ['Electron'] = '#2f3241',
-    ['Qalculate-gtk'] = '#272727',
-    ['DarkPlaces'] = '#000000',
-    ['vlc'] = '#222020',
-    ['Inkscape'] = '#272727',
-    ['Lxappearance'] = '#121212',
-    ['firefox'] = '#1c1b22ff',
-    ['Spotify'] = '#121212',
-    ['feh'] = '#646464',
-    ['discord'] = '#36393f',
-    ['qt5ct'] = '#222020',
-    ['Pcmanfm'] = '#272727',
-    ['XTerm'] = '#ffffff',
-    ['gammy'] = '#1b1c27',
-    ['Gimp-2.10'] = '#484848',
     ['Alacritty'] = '#080808cc',
+    ['qt5ct'] = '#222020',
+    ['code-oss'] = '#272727',
+    ['Qalculate-gtk'] = '#272727',
+    ['XTerm'] = '#ffffff',
     ['VSCodium'] = '#1e1e1e',
-    ['Nitrogen'] = '#121212'
+    ['Spotify'] = '#070707',
+    ['com.github.joseexposito.touche'] = '#ebebebff',
+    ['vlc'] = '#222020',
+    ['Nitrogen'] = '#121212',
+    ['Inkscape'] = '#272727',
+    ['Electron'] = '#2f3241',
+    ['DarkPlaces'] = '#000000',
+    ['discord'] = '#36393f',
+    ['matplotlib'] = '#232121',
+    ['Pcmanfm'] = '#272727',
+    ['feh'] = '#646464',
+    ['Lxappearance'] = '#121212',
+    ['Libfm-pref-apps'] = '#121212',
+    ['Gimp-2.10'] = '#484848',
+    ['gammy'] = '#1b1c27',
+    ['firefox'] = '#1c1b22ff'
 }
 
 return titlebar_client_colors
