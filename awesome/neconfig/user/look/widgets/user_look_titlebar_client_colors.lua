2b8acc3d3d0ada25642f28685ff6c654332cbc30
nenikitov <nenikitov@mail.ru>
2022-03-02 13:00:28 -0500

    Removed unecessary applications from the color list

--- awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
+++ awesome/neconfig/user/look/widgets/user_look_titlebar_client_colors.lua
@@ -7,24 +7,18 @@ local titlebar_client_colors = {
     ['com.github.joseexposito.touche'] = '#ebebebff',
     ['DarkPlaces'] = '#000000',
     ['discord'] = '#36393f',
-    ['Electron'] = '#2f3241',
     ['feh'] = '#646464',
     ['firefox'] = '#1c1b22ff',
-    ['fontforge'] = '#edeceb',
     ['gammy'] = '#1b1c27',
     ['Gimp-2.10'] = '#484848',
     ['Inkscape'] = '#272727',
-    ['Libfm-pref-apps'] = '#121212',
     ['Lxappearance'] = '#121212',
-    ['matplotlib'] = '#232121',
     ['Nitrogen'] = '#121212',
     ['Pcmanfm'] = '#272727',
     ['Qalculate-gtk'] = '#272727',
     ['qt5ct'] = '#222020',
     ['Spotify'] = '#070707',
     ['vlc'] = '#222020',
-    ['VSCodium'] = '#1e1e1e',
-    ['XTerm'] = '#ffffff'
 }
 
 return titlebar_client_colors
