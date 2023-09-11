42e160aa531c07342f5600d03220acefb9562340
nenikitov <nenikitov@mail.ru>
2023-09-10 20:29:24 -0400

    Moved common properties for future modules

---
+++ ags/bar/module-type.ts
@@ -0,0 +1,3 @@
+interface Module {
+  vertical: boolean;
+}
--- ags/bar/modules/workspaces-types.ts
+++ ags/bar/modules/workspaces-types.ts
@@ -1,9 +1,8 @@
-interface WorkspacesConfig {
+interface WorkspacesConfig extends Module {
   format: (workspace: Workspace) => string;
   formatTooltip: (workspace: Workspace) => string;
   hideEmpty: boolean;
   allMonitors: boolean;
-  vertical: boolean;
 }
 
 interface WorkspacesArgs extends Partial<WorkspacesConfig> {
