b902c56c13507840fb8f467993334aae95f2d481
nenikitov <nenikitov@mail.ru>
2023-09-13 13:15:06 -0400

    Set up rust project

---
+++ ags/scripts/keyboard-state/src/main.rs
@@ -0,0 +1,5 @@
+mod args;
+
+fn main() {
+    println!("Hello, world!");
+}
--- ags/services/keyboard-state.js
+++ ags/services/keyboard-state.js
@@ -2,12 +2,12 @@ import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
 import Variable from 'resource:///com/github/Aylur/ags/variable.js';
 import { seconds } from '../utils/time.js';
 
-const KeyboardCaps = Variable(/** @type {LedState} */ (0), {
-  poll: [seconds(0.1), () => {}],
+const KeyboardCaps = Variable(/** @type {LedState} */(0), {
+  poll: [seconds(0.1), () => { }],
 });
 
 const Keyboard = Variable(
-  /** @type {KeyboardState} */ ({
+  /** @type {KeyboardState} */({
     layout: '',
     submap: '',
     locks: {
