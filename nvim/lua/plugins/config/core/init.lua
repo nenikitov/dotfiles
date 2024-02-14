02170ad27eca325c6564383e5200ba67e0f35ea3
nenikitov <nenikitov@mail.ru>
2024-02-14 16:47:12 -0500

    Migrated to core folder

--- nvim/lua/plugins/config/core/init.lua
+++ nvim/lua/plugins/config/core/init.lua
@@ -1,6 +1,5 @@
 return {
     { import = 'plugins.config.core.treesitter' },
     { import = 'plugins.config.core.lsp' },
-    { import = 'plugins.config.core.format' },
-    { import = 'plugins.config.core.lint' },
+    { import = 'plugins.config.core.completion' },
 }
