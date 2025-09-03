e384a599146b482db30c9f2259308a7f45c199d9
nenikitov <nenikitov@mail.ru>
2025-09-03 19:53:27 -0400

    feat: scan host dir

--- flake.nix
+++ flake.nix
@@ -27,24 +27,37 @@
     nixpkgs,
     ...
   } @ inputs: let
-    customNamespace = "_ne";
-
     lib = nixpkgs.lib;
     libModule = inputs.moduleUtils.lib;
     libHomeManager = inputs.homeManager.lib;
     libFlake = inputs.flakeUtils.lib;
 
-    userHosts = [
-      {userName = "nenikitov"; hostName = "nenikitov-pc-nix";}
-      {userName = "nenikitov"; hostName = "nenikitov-laptop-nix";}
+    customNamespace = "_ne";
+
+    hostsDir = "${self}/hosts";
+    hosts = lib.pipe hostsDir [
+      builtins.readDir
+      (lib.concatMapAttrs (p: t:
+        if t == "directory" then { "${p}" = p; }
+        else if t == "regular" && lib.hasSuffix ".nix" p then { "${lib.removeSuffix ".nix" p}" = p; }
+        else {}
+      ))
+      (builtins.mapAttrs (h: p: let
+        parsed = builtins.split "@" h;
+      in {
+        module = "${hostsDir}/${p}";
+        userName = builtins.elemAt parsed 0;
+        hostName = builtins.elemAt parsed 1;
+      }))
     ];
-    mkHome = system: {userName, hostName}:
+
+    mkHome = system: {module, userName, hostName}:
       libHomeManager.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${system};
         modules = [
           inputs.niri.homeModules.niri
           (self.homeManagerModules.default {namespace = customNamespace;})
-          "${self}/hosts/${hostName}"
+          module
         ];
         extraSpecialArgs = {
           inherit inputs userName hostName customNamespace;
@@ -52,16 +65,7 @@
       };
   in
     libFlake.eachSystem libFlake.allSystems (system: {
-      packages.homeConfigurations =
-        lib.pipe
-        userHosts
-        [
-          (builtins.map ({userName, hostName}@userHost: {
-            name = "${userName}@${hostName}";
-            value = mkHome system userHost;
-          }))
-          builtins.listToAttrs
-        ];
+      packages.homeConfigurations = builtins.mapAttrs (_: mkHome system) hosts;
     })
     //
     {
