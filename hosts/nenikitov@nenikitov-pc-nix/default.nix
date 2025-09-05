55690c7b722e76f163b910a495d721919c9dbfbb
nenikitov <nenikitov@mail.ru>
2025-09-05 18:37:51 -0400

    feat(niri): make config work for main pc

--- hosts/nenikitov@nenikitov-pc-nix/default.nix
+++ hosts/nenikitov@nenikitov-pc-nix/default.nix
@@ -1,91 +1,43 @@
 { config, pkgs, userName, customNamespace, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = userName;
-  home.homeDirectory = "/home/${userName}";
+  # Do not change!
+  # Corresponds to the first home-manager version
+  home.stateVersion = "25.05";
 
-  nixpkgs.config.allowUnfree = true;
+  "${customNamespace}" = {
+    profiles.graphical.enable = true;
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+    settings.monitors = {
+      "Ancor Communications Inc MG248 G9LMQS024781" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 143.981;
+        };
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
+      "ASUSTek COMPUTER INC VG245 KCLMQS015128" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 75.001;
+        };
+        position = {
+          x = 1920;
+          y = 0;
+        };
+      };
+    };
+  };
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     discord
     fastfetch
     neovim
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    ripgrep
   ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/nenikitov/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.alacritty.enable = true;
-
-  "${customNamespace}" = {
-    programs = {
-      git.enable = true;
-      librewolf.enable = true;
-      niri.enable = true;
-    };
-  };
 }
