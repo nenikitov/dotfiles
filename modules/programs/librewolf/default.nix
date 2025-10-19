910979a60beadd525a2635136de47c1b479b8993
nenikitov <nenikitov@mail.ru>
2025-10-19 02:48:53 -0400

    feat(librewolf): figure out the solution for search engines

--- modules/programs/librewolf/default.nix
+++ modules/programs/librewolf/default.nix
@@ -1,4 +1,4 @@
-{libModule, ...}:
+{libModule, pkgs, ...}:
 # {
 #   imports = [ ./search-engines.nix ];
 # }
@@ -11,12 +11,22 @@ libModule.mkEnableModule {
 
     programs.librewolf = {
       enable = true;
+      package = pkgs.librewolf.override {
+        extraPolicies = {
+          # HACK: This breaks force loading of search engines from policies,
+          # which allows setting the default search engine with `profile.<profile>.search.default`.
+          SearchEngines.Default = null;
+        };
+      };
       profiles.default = {
+        name = "Default";
         isDefault = true;
+
         search = {
           force = true;
-          default = "google";
+          default = "wikipedia";
         };
+
         settings = {
           "webgl.disabled" = false;
         };
