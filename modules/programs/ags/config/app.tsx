add95c823283765f1c4ffad7d16e4b39950fbf72
nenikitov <nenikitov@mail.ru>
2026-01-28 23:21:57 -0500

    refactor(ags): remove ags

--- modules/programs/ags/config/app.tsx
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S ags run
-
-import app from "ags/gtk4/app"
-import { Windows } from "./windows/index.tsx"
-
-app.start({
-  instanceName: "ne-shell",
-  main: Windows,
-})
--- modules/programs/niri/default.nix
+++ modules/programs/niri/default.nix
@@ -18,7 +18,6 @@ libModule.mkEnableModule {
     # TODO: Figure out module dependencies specification
     ${namespace} = {
       programs = {
-        ags.enable = true;
         awww.enable = true;
       };
     };
