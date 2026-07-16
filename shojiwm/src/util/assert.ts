b2be42f5ea04cfc8e4e886421c798cde4325f39f
nenikitov <nenikitov@mail.ru>
2026-07-16 17:02:30 -0400

    wip: add focus list for workspaces

--- shojiwm/src/util/assert.ts
+++ shojiwm/src/util/assert.ts
@@ -1,4 +1,4 @@
-export function assertType<T>(value: unknown): asserts value is T {}
+export function assertType<T>(value: unknown, message?: string): asserts value is T {}
 
 export function unreachable(message?: string): never {
   throw new Error(message);
--- shojiwm/src/util/focus-list.ts
+++ shojiwm/src/util/focus-list.ts
@@ -1,16 +1,27 @@
 import { assertType } from "./assert";
+import { clamp } from "./math";
 
-export class FocusList<T> {
-  private readonly inner: T[] = [];
-  private active: number | null = null;
+export class FocusListStrict<T> {
+  private readonly inner: T[];
+  private active: number | null;
+
+  public constructor(items: T[] = []) {
+    this.inner = items;
+    this.active = items.length > 0 ? 0 : null;
+  }
 
   public get items(): readonly T[] {
     return this.inner;
   }
 
+  public get length(): number {
+    return this.inner.length;
+  }
+
   public getActive(method: "index"): number | null;
   public getActive(method: "object"): T | null;
-  public getActive(method: "index" | "object" = "index"): number | T | null {
+  public getActive(method: "index" | "object"): number | T | null;
+  public getActive(method: "index" | "object"): number | T | null {
     if (method == "index") {
       return this.active;
     } else {
@@ -19,15 +30,18 @@ export class FocusList<T> {
   }
 
   public setActive(method: "first" | "last"): void;
-  public setActive(method: "index", value: number | null): void;
-  public setActive(method: "relative", value: number | null): void;
-  public setActive(method: "object", value: T | null): void;
+  public setActive(method: "index", value: number): void;
+  public setActive(method: "relative", value: number): void;
+  public setActive(method: "object", value: T): void;
   public setActive(
     method: "first" | "last" | "index" | "relative" | "object",
-    value?: number | T | null,
+    value?: number | T,
+  ): void;
+  public setActive(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T,
   ): void {
-    if (value === null || this.inner.length === 0) {
-      this.active = null;
+    if (this.inner.length === 0 || this.active === null) {
       return;
     }
 
@@ -42,32 +56,42 @@ export class FocusList<T> {
         break;
       }
       case "index": {
-        assertType<number | null>(value);
+        assertType<number>(
+          value,
+          "When method is index, value is always a number",
+        );
         target = value >= 0 ? value : value + this.inner.length;
         break;
       }
       case "relative": {
-        assertType<number | null>(value);
-        target = (this.active ?? 0) + value;
+        assertType<number>(
+          value,
+          "When method is relative, value is always a number",
+        );
+        target = this.active + value;
         break;
       }
       case "object": {
-        assertType<T>(value);
+        assertType<T>(value, "When method is object, value is always T");
         const index = this.inner.indexOf(value);
         target = index >= 0 ? index : null;
         break;
       }
     }
 
-    this.active =
-      target !== null
-        ? Math.max(0, Math.min(this.inner.length - 1, target))
-        : null;
+    if (target !== null) {
+      this.active = Math.max(0, Math.min(this.inner.length - 1, target));
+    }
   }
 
   public push(value: T, method: "first" | "last"): void;
   public push(value: T, method: "index", index: number): void;
   public push(value: T, method: "relative", index: number): void;
+  public push(
+    value: T,
+    method: "first" | "last" | "index" | "relative",
+    index?: number,
+  ): void;
   public push(
     value: T,
     method: "first" | "last" | "index" | "relative",
@@ -84,26 +108,32 @@ export class FocusList<T> {
         break;
       }
       case "index": {
-        assertType<number>(index);
+        assertType<number>(
+          index,
+          "When method is index, index is always a number",
+        );
         target = index >= 0 ? index : index + this.inner.length;
         break;
       }
       case "relative": {
-        assertType<number>(index);
+        assertType<number>(
+          index,
+          "When method is index, index is always a number",
+        );
         target = (this.active ?? 0) + index;
         break;
       }
     }
 
     // Insert
-    target = this.clampIndex(target);
+    target = clamp(target, 0, this.length);
     this.inner.splice(target, 0, value);
 
     // Update active
     if (this.active === null) {
       this.active = 0;
     } else if (target <= this.active) {
-      this.active = this.clampIndex(this.active + 1);
+      this.active = clamp(this.active + 1, 0, this.length - 1);
     }
   }
 
@@ -114,8 +144,12 @@ export class FocusList<T> {
   public remove(
     method: "first" | "last" | "index" | "relative" | "object",
     value?: number | T,
+  ): T | null;
+  public remove(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T,
   ): T | null {
-    if (this.inner.length === 0) {
+    if (this.inner.length === 0 || this.active === null) {
       return null;
     }
 
@@ -147,7 +181,7 @@ export class FocusList<T> {
       }
     }
 
-    if (target === null) {
+    if (target === null || target < 0 || target >= this.length) {
       return null;
     }
 
@@ -158,13 +192,183 @@ export class FocusList<T> {
     if (this.inner.length === 0) {
       this.active = null;
     } else if (this.active !== null && target <= this.active) {
-      this.active = this.clampIndex(this.active - 1);
+      this.active = clamp(this.active - 1, 0, this.length - 1);
     }
 
     return removed;
   }
+}
 
-  private clampIndex(index: number): number {
-    return Math.max(0, Math.min(this.inner.length, index));
+export class FocusListDynamic<T> {
+  private inner: FocusListStrict<T>;
+  private readonly factory: () => T;
+  private readonly canCleanup: (value: T) => boolean;
+
+  public constructor(
+    factory: () => T,
+    canCleanup: (value: T) => boolean,
+    items: T[] = [],
+  ) {
+    this.inner = new FocusListStrict(items);
+    this.factory = factory;
+    this.canCleanup = canCleanup;
+    this.ensureCapacity();
+  }
+
+  public get items(): readonly T[] {
+    return this.inner.items;
+  }
+
+  public get length(): number {
+    return this.inner.length;
+  }
+
+  public getActive(method: "index"): number;
+  public getActive(method: "object"): T;
+  public getActive(method: "index" | "object"): number | T;
+  public getActive(method: "index" | "object"): number | T {
+    const result = this.inner.getActive(method);
+    assertType<number | T>(
+      result,
+      "We always keep the length of the inner list at least 1 (ie has active)",
+    );
+    return result;
+  }
+
+  public setActive(method: "first" | "last"): void;
+  public setActive(method: "index", value: number): void;
+  public setActive(method: "relative", value: number): void;
+  public setActive(method: "object", value: T): void;
+  public setActive(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T,
+  ): void;
+  public setActive(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T | null,
+  ): void {
+    let target: number | null = null;
+    switch (method) {
+      case "first": {
+        target = 0;
+        break;
+      }
+      case "last": {
+        target = this.inner.length - 1;
+        break;
+      }
+      case "index": {
+        assertType<number>(
+          value,
+          "When method is index, value is always a number",
+        );
+        target = value >= 0 ? value : value + this.inner.length;
+        break;
+      }
+      case "relative": {
+        assertType<number>(
+          value,
+          "When method is relative, value is always a number",
+        );
+        target = this.getActive("index") + value;
+        break;
+      }
+      case "object": {
+        assertType<T>(value, "When method is object, value is always T");
+        const index = this.items.indexOf(value);
+        target = index >= 0 ? index : null;
+        break;
+      }
+    }
+
+    if (target !== null) {
+      this.ensureCapacity(target + 1);
+      this.inner.setActive("index", target);
+      this.cleanupTrailing();
+    }
+  }
+
+  public push(value: T, method: "first" | "last"): void;
+  public push(value: T, method: "index", index: number): void;
+  public push(value: T, method: "relative", index: number): void;
+  public push(
+    value: T,
+    method: "first" | "last" | "index" | "relative",
+    index?: number,
+  ): void;
+  public push(
+    value: T,
+    method: "first" | "last" | "index" | "relative",
+    index?: number,
+  ): void {
+    let target: number = 0;
+    switch (method) {
+      case "first": {
+        target = 0;
+        break;
+      }
+      case "last": {
+        target = this.inner.length;
+        break;
+      }
+      case "index": {
+        assertType<number>(
+          index,
+          "When method is index, index is always a number",
+        );
+        target = index >= 0 ? index : index + this.inner.length;
+        break;
+      }
+      case "relative": {
+        assertType<number>(
+          index,
+          "When method is index, index is always a number",
+        );
+        target = this.getActive("index") + index;
+        break;
+      }
+    }
+    target = clamp(target, 0);
+    this.ensureCapacity(target);
+    this.inner.push(value, "index", target);
+    this.cleanupTrailing();
+  }
+
+  public remove(method: "first" | "last"): T;
+  public remove(method: "index", value: number): T | null;
+  public remove(method: "relative", value: number): T | null;
+  public remove(method: "object", value: T): T | null;
+  public remove(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T,
+  ): T | null;
+  public remove(
+    method: "first" | "last" | "index" | "relative" | "object",
+    value?: number | T,
+  ): T | null {
+    const result = this.inner.remove(method, value);
+    this.ensureCapacity();
+    this.cleanupTrailing();
+    return result;
+  }
+
+  private ensureCapacity(length: number = 1) {
+    while (this.length < length) {
+      this.inner.push(this.factory(), "last");
+    }
+  }
+
+  private cleanupTrailing() {
+    for (
+      let i = this.length - 1;
+      i > this.getActive("index") && this.length > 1;
+      i--
+    ) {
+      if (this.canCleanup(this.items[i])) {
+        this.inner.remove("index", i);
+      } else {
+        break;
+      }
+    }
   }
 }
