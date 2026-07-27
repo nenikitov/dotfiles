a88ef3b79973be84748ac5a072525a593e9aa839
nenikitov <nenikitov@mail.ru>
2026-07-27 19:24:40 -0400

    chore: restart everything

--- shojiwm/src/index.tsx
+++ shojiwm/src/index.tsx
@@ -1,10 +1,17 @@
-import { writeFileSync } from "node:fs";
-
 import {
+  ClientWindow,
   COMPOSITOR,
+  computed,
+  createWindowState,
+  cubicBezier,
+  ManagedWindow,
+  seconds,
+  WindowBorder,
   type DisplayConfigDraft,
+  type OutputInfo,
+  type WaylandWindow,
+  type WindowPosition,
 } from "shoji_wm";
-import { WindowManager } from "./window-manager/columns";
 
 export function notify(value: any) {
   COMPOSITOR.process.spawn({
@@ -28,65 +35,282 @@ COMPOSITOR.process.once("dunst", {
   runPolicy: "once-per-session",
 });
 
-const wm = new WindowManager();
-
-// Launcher
-COMPOSITOR.key.bind("terminal", "Super+Return", () => {
-  COMPOSITOR.process.spawn({ command: ["alacritty"] });
+COMPOSITOR.cursor.configure({
+  theme: "Adwaita",
+  size: 24,
 });
-COMPOSITOR.key.bind("launcher", "Super+Shift+Return", () => {
-  COMPOSITOR.process.spawn({ command: ["rofi", "-show", "drun"] });
-});
-
-COMPOSITOR.key.bind("debug", "Super+D", () => {
-  notify("Debug - Writing");
-
-  const value: any = ["wm", wm];
-
-  writeFileSync(
-    "/home/nenikitov/.config/shojiwm/debug.json",
-    JSON.stringify(value, undefined, 2),
-  );
-
-  notify(value);
-
-  notify("Debug - Written");
-});
-
-COMPOSITOR.pointer.bindWindowMoveModifier("Super");
-COMPOSITOR.pointer.bindWindowResizeModifier("Super");
 
 COMPOSITOR.output.configure((context) => {
   const display: DisplayConfigDraft = {};
 
   display["DP-1"] = {
     mode: "extend",
-    resolution: { width: 1920, height: 1080, refreshRate: 144 },
+    resolution: "best",
     position: "auto",
+    scale: 1.0,
   };
   display["HDMI-A-1"] = {
     mode: "extend",
-    resolution: { width: 1920, height: 1080, refreshRate: 75 },
+    resolution: "best",
     position: "auto",
+    scale: 1.0,
   };
 
   return display;
 });
 
-COMPOSITOR.input.configure((input, _context) => {
+COMPOSITOR.input.configure((input, context) => {
   input.global = {
-    touchpad: {
-      tapToClick: true,
-      naturalScroll: true,
-      scrollMethod: "twoFinger",
-      disableWhileTyping: true,
-      scrollFactor: 0.3,
-    },
     pointer: {
-      pointerAccel: 0.0,
       accelProfile: "flat",
+      pointerAccel: 0,
     },
   };
+});
+
+const MOD = "Super";
+
+COMPOSITOR.pointer.bindWindowMoveModifier(`${MOD}`);
+COMPOSITOR.pointer.bindWindowResizeModifier(`${MOD}`);
+
+COMPOSITOR.key.bind("terminal", `${MOD}+Return`, () => {
+  COMPOSITOR.process.spawn({
+    command: ["alacritty"],
+  });
+});
+COMPOSITOR.key.bind("launcher", `${MOD}+Shift+Return`, () => {
+  COMPOSITOR.process.spawn({
+    command: ["rofi", "-show", "drun"],
+  });
+});
+
+const states = {
+  rect: createWindowState<WindowPosition>("rect", {
+    default: () => ({ x: 0, y: 0, width: 0, height: 0 }),
+  }),
+} as const;
+
+const strut = 8;
+const gap = 8;
+const openPercent = 0.9;
+
+class Output {
+  handle: OutputInfo;
+  windows: WaylandWindow[];
+
+  constructor(handle: OutputInfo) {
+    this.handle = handle;
+    this.windows = [];
+  }
+
+  get rect(): WindowPosition | undefined {
+    if (this.handle.resolution === undefined) {
+      return undefined;
+    }
+
+    return {
+      x: this.handle.position.x,
+      y: this.handle.position.y,
+      width: this.handle.resolution.width / this.handle.scale,
+      height: this.handle.resolution.height / this.handle.scale,
+    };
+  }
+
+  addWindow(window: WaylandWindow) {
+    const exists = this.windows.some((inWindows) => inWindows.id === window.id);
+    if (exists) {
+      return;
+    }
+
+    this.windows.push(window);
+    this.updateLayout();
+  }
+
+  removeWindow(window: WaylandWindow): boolean {
+    const index = this.windows.findIndex(
+      (inWindows) => inWindows.id === window.id,
+    );
+    if (index < 0) {
+      return false;
+    }
+
+    this.windows.splice(index, 1);
+    this.updateLayout();
+
+    return true;
+  }
+
+  updateLayout() {
+    const rect = this.rect;
+
+    if (this.windows.length === 0 || rect === undefined) {
+      return;
+    }
+
+    const easing = cubicBezier(0.1, 0.9, 0.2, 1);
+    const duration = seconds(0.25);
+
+    const tile = {
+      x: rect.x + strut,
+      y: rect.y + strut,
+      width:
+        (rect.width - 2 * strut - (this.windows.length - 1) * gap) /
+        this.windows.length,
+      height: rect.height - 2 * strut,
+    };
+
+    for (const window of this.windows) {
+      let from: WindowPosition = window.state[states.rect].peek();
+      if (
+        from.x === 0 &&
+        from.y === 0 &&
+        from.width === 0 &&
+        from.height === 0
+      ) {
+        from = {
+          x: tile.x + tile.width * (1 - openPercent),
+          y: tile.y + tile.height * (1 - openPercent),
+          width: tile.width * (-1 + 2 * openPercent),
+          height: tile.height * (-1 + 2 * openPercent),
+        };
+      }
+      const to = { ...tile };
+
+      window.state[states.rect].set(to);
+      window.scheduleAnimation({
+        channel: "reflow",
+        rect: {
+          from,
+          to,
+          easing,
+          duration,
+        },
+      });
+      tile.x += tile.width + gap;
+    }
+  }
+}
+
+const outputs: Output[] = [];
+let activeOutput: number | null = null;
+
+function updateOutputs(
+  added: OutputInfo[],
+  changed: OutputInfo[] = [],
+  removed: OutputInfo[] = [],
+) {
+  for (const a of added) {
+    const output = new Output(a);
+    outputs.push(output);
+    if (activeOutput === null) {
+      activeOutput = 0;
+    }
+  }
+
+  for (const c of changed) {
+    const output = outputs.find((o) => o.handle.name === c.name);
+    if (output === undefined) {
+      continue;
+    }
+
+    output.handle = c;
+    output.updateLayout();
+  }
+
+  for (const r of removed) {
+    const index = outputs.findIndex((o) => o.handle.name === r.name);
+    if (index < 0) {
+      continue;
+    }
+
+    for (const window of outputs[index].windows) {
+      window.close();
+    }
+    outputs.splice(index, 1);
+
+    if (outputs.length === 0) {
+      activeOutput = null;
+    } else if (activeOutput !== null) {
+      activeOutput = Math.min(activeOutput, outputs.length - 1);
+    }
+  }
+}
+
+COMPOSITOR.event.onPointerMove((event) => {
+  const index = outputs.findIndex((o) => o.handle.name === event.outputName);
+  if (index < 0) {
+    return;
+  }
+
+  activeOutput = index;
+});
+
+COMPOSITOR.event.onPointerMove((event) => {
+  if (event.target.kind !== "window") {
+    return;
+  }
+  const output = outputs.find((o) => o.handle.name === event.outputName);
+  if (output === undefined) {
+    return;
+  }
+
+  const windowId = event.target.windowId;
+  const window = output.windows.find((w) => w.id === windowId);
+  if (window === undefined) {
+    return;
+  }
+
+  window.focus()
+});
+
+updateOutputs(COMPOSITOR.output.outputs);
+COMPOSITOR.event.onOutputChange((event) => {
+  updateOutputs(event.added, event.changed, event.removed);
+});
+
+COMPOSITOR.event.onFirstCommit((window) => {
+  if (window.id === "__warmup__") {
+    return;
+  }
+
+  if (activeOutput !== null) {
+    outputs[activeOutput].addWindow(window);
+  }
+});
+
+COMPOSITOR.event.onStartClose((window) => {
+  for (const output of outputs) {
+    if (output.removeWindow(window)) {
+      return;
+    }
+  }
+});
+
+COMPOSITOR.event.onWindowResize(() => {});
+COMPOSITOR.event.onWindowMove(() => {});
+
+COMPOSITOR.window.composition = (window) => {
+  const forceRectSize = computed(() => {
+    return window.isResizable() && !window.isTransient();
+  });
+  const borderColor = window.isFocused((focused) =>
+    focused ? "#ff0000" : "#000000",
+  );
+
+  return (
+    <ManagedWindow
+      rect={window.state[states.rect]}
+      forceRectSize={forceRectSize}
+    >
+      <WindowBorder style={{ border: { px: 2, color: borderColor } }}>
+        <ClientWindow />
+      </WindowBorder>
+    </ManagedWindow>
+  );
+};
+
+COMPOSITOR.key.bind("debug", `${MOD}+D`, () => {
+  notify(["ACTIVE OUTPUT", activeOutput]);
 });
 
 export default COMPOSITOR;
--- shojiwm/src/util/math.ts
+++
@@ -1,9 +0,0 @@
-export function clamp(value: number, min?: number, max?: number) {
-  if (min !== undefined) {
-    value = Math.max(value, min);
-  }
-  if (max !== undefined) {
-    value = Math.min(value, max);
-  }
-  return value;
-}
